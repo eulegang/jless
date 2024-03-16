306a23fa3cd52ac235f4133bab358e7300f54920
eulegang <eulegang@eulegang.dev>
2024-03-16 09:29:23 -0500

    moving term out of system

--- src/system.zig
+++ src/system.zig
@@ -1,52 +1,31 @@
 const std = @import("std");
-const tag = @import("builtin").os.tag;
 const JQ = @import("jq.zig").JQ;
 
 const Inputs = @import("inputs.zig").Inputs;
+const Term = @import("term.zig").Term;
 
 pub const System = struct {
     inputs: Inputs,
-    term: std.os.termios,
+    term: Term,
 
     filter: ?JQ,
 
     pub fn init(file: ?[]const u8) !System {
         const inputs = try Inputs.init(file);
+        const term = Term.init();
 
         return System{
             .inputs = inputs,
-            .term = undefined,
+            .term = term,
             .filter = null,
         };
     }
 
     pub fn setup(self: *@This()) !void {
-        switch (tag) {
-            .linux => {
-                _ = std.os.linux.tcgetattr(1, &self.term);
-                var term = self.term;
-
-                term.lflag.ECHO = false;
-                term.lflag.ICANON = false;
-                term.lflag.ISIG = false;
-                term.lflag.IEXTEN = false;
-
-                term.oflag.OPOST = false;
-
-                term.iflag.IXON = false;
-                term.iflag.BRKINT = false;
-                term.iflag.INPCK = false;
-                term.iflag.ISTRIP = false;
-
-                _ = std.os.linux.tcsetattr(1, .FLUSH, &term);
-            },
-
-            else => {},
-        }
+        self.term.raw();
     }
 
     pub fn tick(self: *@This()) !bool {
-        _ = try std.os.write(1, "tick\r\n");
         const event = try self.inputs.event();
         switch (event) {
             .line => |line| {
@@ -67,13 +46,6 @@ pub const System = struct {
 
     pub fn close(self: *@This()) void {
         self.inputs.close();
-
-        switch (tag) {
-            .linux => {
-                _ = std.os.linux.tcsetattr(1, .FLUSH, &self.term);
-            },
-
-            else => {},
-        }
+        self.term.deinit();
     }
 };
---
+++ src/term.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const tag = @import("builtin").os.tag;
+
+/// Manages the termios for various platforms
+pub const Term = struct {
+    term: std.os.termios,
+
+    pub fn init() Term {
+        var term: std.os.termios = undefined;
+        _ = std.os.linux.tcgetattr(1, &term);
+
+        return Term{ .term = term };
+    }
+
+    pub fn raw(self: Term) void {
+        switch (tag) {
+            .linux => {
+                var mod = self.term;
+
+                mod.lflag.ECHO = false;
+                mod.lflag.ICANON = false;
+                mod.lflag.ISIG = false;
+                mod.lflag.IEXTEN = false;
+
+                mod.oflag.OPOST = false;
+
+                mod.iflag.IXON = false;
+                mod.iflag.BRKINT = false;
+                mod.iflag.INPCK = false;
+                mod.iflag.ISTRIP = false;
+
+                _ = std.os.linux.tcsetattr(1, .FLUSH, &mod);
+            },
+
+            else => {},
+        }
+    }
+
+    pub fn deinit(self: Term) void {
+        switch (tag) {
+            .linux => {
+                _ = std.os.linux.tcsetattr(1, .FLUSH, &self.term);
+            },
+
+            else => {},
+        }
+    }
+};
