d73684e864c87ef9b807cb42178398a3e71a1af1
eulegang <eulegang@eulegang.dev>
2024-03-16 09:14:07 -0500

    stripping out watch stuff (didn't work anyway)

--- src/inputs.zig
+++ src/inputs.zig
@@ -31,14 +31,8 @@ pub const Inputs = struct {
 
     init_read: bool,
 
-    driver: switch (tag) {
-        .linux => Epoll,
-        .macos => Kqueue,
-        else => @compileError("Only suports linux and mac"),
-    },
-
     pub fn init(file: ?[]const u8) !Self {
-        var user: fd_t = 0;
+        const user: fd_t = 0;
         var src: fd_t = 0;
 
         if (file) |f| {
@@ -47,54 +41,37 @@ pub const Inputs = struct {
                 .CLOEXEC = true,
                 .NONBLOCK = true,
             }, 0o755);
-
-            try set_nonblocking(user);
         } else {
-            if (std.os.isatty(src)) {
-                return InputsError.NoSource;
-            }
-
-            try set_nonblocking(src);
-            user = try std.os.open("/dev/tty", .{
-                .ACCMODE = .RDONLY,
-                .CLOEXEC = true,
-                .NONBLOCK = true,
-            }, 0o755);
+            return InputsError.NoSource;
         }
 
         const mirror = try Mirror(0x2000).new();
 
-        const driver = switch (tag) {
-            .linux => try Epoll.init(user, src),
-            .macos => try Kqueue.init(user, src),
-            else => @compileError("Only supports mac or linux"),
-        };
-
         return Inputs{
             .user = user,
             .src = src,
-
             .mirror = mirror,
-
             .init_read = true,
-
-            .driver = driver,
         };
     }
 
     pub fn event(self: *Self) !Event {
         while (true) {
-            // TODO: something seems wrong here
             if (self.init_read) {
-                _ = self.mirror.read_fd(self.src) catch |err| switch (err) {
+                const x = self.mirror.read_fd(self.src) catch |err| switch (err) {
                     error.WouldBlock => {
                         self.init_read = false;
+                        continue;
                     },
 
                     else => {
                         return err;
                     },
                 };
+
+                if (x == 0) {
+                    self.init_read = false;
+                }
             }
 
             if (self.mirror.len() != 0) {
@@ -108,48 +85,25 @@ pub const Inputs = struct {
                 }
             }
 
-            if (try self.driver.next()) |fd| {
-                std.debug.print("what? {d} ({}, {})\r\n", .{ fd, self.user, self.src });
-                if (fd == self.user) {
-                    std.debug.print("reading? {d}\r\n", .{self.user});
-                    var buf: [4]u8 = undefined;
-                    const len = try std.os.read(fd, &buf);
+            var buf: [4]u8 = undefined;
+            const len = try std.os.read(self.user, &buf);
 
-                    if (len == 0) continue;
+            if (len == 0) continue;
 
-                    const ch = buf[0];
+            const ch = buf[0];
 
-                    if (len == 1) {
-                        switch (ch) {
-                            '\x1b' => return .{ .input = .Escape },
-                            '\n' => return .{ .input = .Select },
+            if (len == 1) {
+                switch (ch) {
+                    '\x1b' => return .{ .input = .Escape },
+                    '\n' => return .{ .input = .Select },
 
-                            'q' => return .{ .input = .Quit },
-                            'j' => return .{ .input = .Down },
-                            'k' => return .{ .input = .Up },
+                    'q' => return .{ .input = .Quit },
+                    'j' => return .{ .input = .Down },
+                    'k' => return .{ .input = .Up },
 
-                            else => {
-                                std.debug.print("unhandled key {d}\r\n", .{ch});
-                            },
-                        }
-                    }
-                } else if (fd == self.src) {
-                    _ = self.mirror.read_fd(self.src) catch |err| switch (err) {
-                        error.WouldBlock => continue,
-                        else => return err,
-                    };
-
-                    const avail = self.mirror.buffer();
-
-                    if (std.mem.indexOf(u8, avail, "\n")) |index| {
-                        const line = avail[0..index];
-
-                        _ = self.mirror.drop(index + 1);
-
-                        return Event{ .line = line };
-                    }
-                } else {
-                    unreachable;
+                    else => {
+                        std.debug.print("unhandled key {d}\r\n", .{ch});
+                    },
                 }
             }
         }
@@ -160,46 +114,3 @@ pub const Inputs = struct {
         std.os.close(self.src);
     }
 };
-
-const WatcherError = error{watcher_create_error};
-
-const epoll_t = @import("inputs/epoll.zig").epoll_t;
-const Epoll = struct {
-    user: fd_t,
-    src: fd_t,
-
-    epoll: epoll_t,
-
-    fn init(user: fd_t, src: fd_t) !@This() {
-        const epoll = try epoll_t.init();
-
-        try epoll.add(user);
-        try epoll.add(src);
-
-        return @This(){
-            .user = user,
-            .src = src,
-            .epoll = epoll,
-        };
-    }
-
-    fn next(self: @This()) !?fd_t {
-        return self.epoll.next();
-    }
-
-    fn deinit(self: @This()) void {
-        self.epoll.deinit();
-    }
-};
-
-const Kqueue = struct {
-    fn init() @This() {
-        return @This(){};
-    }
-};
-
-fn set_nonblocking(fd: fd_t) !void {
-    const NONBLOCK = 1 << @bitOffsetOf(std.os.system.O, "NONBLOCK");
-    const flags = try std.os.fcntl(fd, std.os.F.GETFL, 0);
-    _ = try std.os.fcntl(fd, std.os.F.SETFL, flags | NONBLOCK);
-}
